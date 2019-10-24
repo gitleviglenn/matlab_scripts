16b007100b5af60c7e3719d415703a6a37fc8010
levi silvers <levi.silvers@stonybrook.edu>
2019-10-24 14:07:01 -0400

    glbmn_toa_fb.m compute the radiative feedbacks at toa, the driver script applies the computations to CAM5 and CAM6 for three different surface temperatures

---
+++ glbmn_toa_fb_driver.m
@@ -0,0 +1,103 @@
+model_generation='CAM6';
+
+source_mod=strcat(source,model_generation,'_GCM_RCE/');
+exp_string=strcat(model_generation,'_GCM_RCE_large295_0D_'); 
+glbmn_toa_fb
+rad_295_array=rad_ts_array;
+
+exp_string=strcat(model_generation,'_GCM_RCE_large300_0D_'); 
+glbmn_toa_fb
+rad_300_array=rad_ts_array;
+
+exp_string=strcat(model_generation,'_GCM_RCE_large305_0D_'); 
+glbmn_toa_fb
+rad_305_array=rad_ts_array;
+
+% compute a Cess-style sensitivity
+cess_a=zeros(5,2625);
+cess_b=zeros(5,2625);
+cess_a=-(3.7*5)./(rad_300_array-rad_295_array);
+cess_b=-(3.7*5)./(rad_305_array-rad_300_array);
+
+figure
+subplot(3,1,1)
+plot(rad_295_array(3,:),'LineWidth',2);
+hold on
+ylabel('W/m2')
+plot(rad_300_array(3,:));
+plot(rad_305_array(3,:));
+title('TOA CRE')
+subplot(3,1,2)
+plot(rad_295_array(1,:),'LineWidth',2);
+hold on
+ylabel('W/m2')
+plot(rad_300_array(1,:));
+plot(rad_305_array(1,:));
+title('TOA R')
+subplot(3,1,3)
+plot(cess_a(1,:));
+ylim([0 3])
+hold on
+ylabel('K')
+plot(cess_b(1,:));
+title('Cess Sensitivity CAM6')
+
+%figure
+%plot(cess_a(1,1:2400))
+%hold on
+%ylim([-7 0])
+%plot(cess_b(1,1:2400),'k')
+%title('Cess Sensitivity CAM6 over first 100 days')
+
+
+model_generation='CAM5';
+
+source_mod=strcat(source,model_generation,'_GCM_RCE/');
+exp_string=strcat(model_generation,'_GCM_RCE_large295_0D_'); 
+glbmn_toa_fb
+rad_295_array=rad_ts_array;
+
+exp_string=strcat(model_generation,'_GCM_RCE_large300_0D_'); 
+glbmn_toa_fb
+rad_300_array=rad_ts_array;
+
+exp_string=strcat(model_generation,'_GCM_RCE_large305_0D_'); 
+glbmn_toa_fb
+rad_305_array=rad_ts_array;
+
+% compute a Cess-style sensitivity
+cess_a=zeros(5,2625);
+cess_b=zeros(5,2625);
+cess_a=-(3.7*5)./(rad_300_array-rad_295_array);
+cess_b=-(3.7*5)./(rad_305_array-rad_300_array);
+
+figure
+subplot(3,1,1)
+plot(rad_295_array(3,:),'LineWidth',2);
+hold on
+plot(rad_300_array(3,:),'LineWidth',2);
+plot(rad_305_array(3,:));
+title('TOA CRE')
+ylabel('W/m2')
+subplot(3,1,2)
+plot(rad_295_array(1,:),'LineWidth',2);
+hold on
+plot(rad_300_array(1,:),'LineWidth',2);
+plot(rad_305_array(1,:));
+ylabel('W/m2')
+title('TOA R')
+subplot(3,1,3)
+plot(cess_a(1,:));
+hold on
+ylim([0 3])
+plot(cess_b(1,:));
+ylabel('K')
+title('Cess Sensitivity CAM5')
+
+%figure
+%plot(cess_a(1,1:2400))
+%ylim([-7 0])
+%hold on
+%plot(cess_b(1,1:2400),'k')
+%title('Cess Sensitivity CAM5 over first 100 days')
+
