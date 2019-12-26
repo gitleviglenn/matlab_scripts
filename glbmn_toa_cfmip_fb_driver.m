e4c90a5e00fc86a26402c96273de95d47fe28936
levi silvers <levi.silvers@stonybrook.edu>
2019-12-26 15:03:42 -0500

    added more variables to analyze to the rce_means.m script and changes to glbmn_toa_fb_driver.m that compute the actual difference in tas whith which the feedback is computed instead of assuming the difference is 4K.

--- glbmn_toa_cfmip_fb_driver.m
+++ glbmn_toa_cfmip_fb_driver.m
@@ -15,6 +15,15 @@
 % to get the numbers for amip we need to play some games becuase i can't write
 % to my own data directory for some u
 
+% compute the change in surface air temperature for the Cess-like experiments
+glbmn_tas_cfmip
+
+delT_amip_p4K=amip_p4K_mts-amip_mts
+delT_amip_m4K=amip_mts-amip_m4K_mts
+delT_amip_fut=amip_F4K_mts-amip_mts
+delT_amip_8K=amip_p4K_mts-amip_m4K_mts
+delT_aqua=aqua_p4K_mts-aqua_mts
+
 % define an array to hold the mean feedback values for each experiment
 rad_fb_array=zeros(7,5); % if the running mean is run.
 
@@ -64,13 +73,13 @@ exp_name='aqua-p4K';
 glbmn_toa_cfmip_fb
 rad_ts_array_aqua_p4K=rad_ts_array;
 
-del_R_aq_p4K       =(mean(rad_ts_array_aqua_p4K(1,:))-mean(rad_ts_array_aqua_control(1,:)))/4;
-del_Rclr_aq_p4K    =(mean(rad_ts_array_aqua_p4K(2,:))-mean(rad_ts_array_aqua_control(2,:)))/4;
-del_cre_aq_p4K    =(mean(rad_ts_array_aqua_p4K(3,:))-mean(rad_ts_array_aqua_control(3,:)))/4;
-del_lwcre_aq_p4K  =(mean(rad_ts_array_aqua_p4K(4,:))-mean(rad_ts_array_aqua_control(4,:)))/4;
-del_swcre_aq_p4K  =(mean(rad_ts_array_aqua_p4K(5,:))-mean(rad_ts_array_aqua_control(5,:)))/4;
-del_lwcs_aq_p4K   =(mean(rad_ts_array_aqua_p4K(6,:))-mean(rad_ts_array_aqua_control(6,:)))/4;
-del_swcs_aq_p4K   =(mean(rad_ts_array_aqua_p4K(7,:))-mean(rad_ts_array_aqua_control(7,:)))/4;
+del_R_aq_p4K      =(mean(rad_ts_array_aqua_p4K(1,:))-mean(rad_ts_array_aqua_control(1,:)))/delT_aqua;
+del_Rclr_aq_p4K   =(mean(rad_ts_array_aqua_p4K(2,:))-mean(rad_ts_array_aqua_control(2,:)))/delT_aqua;
+del_cre_aq_p4K    =(mean(rad_ts_array_aqua_p4K(3,:))-mean(rad_ts_array_aqua_control(3,:)))/delT_aqua;
+del_lwcre_aq_p4K  =(mean(rad_ts_array_aqua_p4K(4,:))-mean(rad_ts_array_aqua_control(4,:)))/delT_aqua;
+del_swcre_aq_p4K  =(mean(rad_ts_array_aqua_p4K(5,:))-mean(rad_ts_array_aqua_control(5,:)))/delT_aqua;
+del_lwcs_aq_p4K   =(mean(rad_ts_array_aqua_p4K(6,:))-mean(rad_ts_array_aqua_control(6,:)))/delT_aqua;
+del_swcs_aq_p4K   =(mean(rad_ts_array_aqua_p4K(7,:))-mean(rad_ts_array_aqua_control(7,:)))/delT_aqua;
 
 %rad_fb_array_str='aqua_p4K;amip_m4K;amip_p4K;amip_p8K;amip_future4K'
 % until the aqua p4K is computed, fill the array with amip m4K
@@ -89,13 +98,13 @@ exp_name='amip-m4K';
 glbmn_toa_cfmip_fb
 rad_ts_array_m4K=rad_ts_array;
 
-delR_m4K      =(mean(rad_ts_array_amip(1,:))-mean(rad_ts_array_m4K(1,:)))/4;
-del_clr_m4K   =(mean(rad_ts_array_amip(2,:))-mean(rad_ts_array_m4K(2,:)))/4;
-del_cre_m4K   =(mean(rad_ts_array_amip(3,:))-mean(rad_ts_array_m4K(3,:)))/4;
-del_lwcre_m4K =(mean(rad_ts_array_amip(4,:))-mean(rad_ts_array_m4K(4,:)))/4;
-del_swcre_m4K =(mean(rad_ts_array_amip(5,:))-mean(rad_ts_array_m4K(5,:)))/4;
-del_lwcs_m4K  =(mean(rad_ts_array_amip(6,:))-mean(rad_ts_array_m4K(6,:)))/4;
-del_swcs_m4K  =(mean(rad_ts_array_amip(7,:))-mean(rad_ts_array_m4K(7,:)))/4;
+delR_m4K      =(mean(rad_ts_array_amip(1,:))-mean(rad_ts_array_m4K(1,:)))/delT_amip_m4K;
+del_clr_m4K   =(mean(rad_ts_array_amip(2,:))-mean(rad_ts_array_m4K(2,:)))/delT_amip_m4K;
+del_cre_m4K   =(mean(rad_ts_array_amip(3,:))-mean(rad_ts_array_m4K(3,:)))/delT_amip_m4K;
+del_lwcre_m4K =(mean(rad_ts_array_amip(4,:))-mean(rad_ts_array_m4K(4,:)))/delT_amip_m4K;
+del_swcre_m4K =(mean(rad_ts_array_amip(5,:))-mean(rad_ts_array_m4K(5,:)))/delT_amip_m4K;
+del_lwcs_m4K  =(mean(rad_ts_array_amip(6,:))-mean(rad_ts_array_m4K(6,:)))/delT_amip_m4K;
+del_swcs_m4K  =(mean(rad_ts_array_amip(7,:))-mean(rad_ts_array_m4K(7,:)))/delT_amip_m4K;
 
 rad_fb_array(1,2)=delR_m4K;
 rad_fb_array(2,2)=del_clr_m4K;
@@ -110,13 +119,13 @@ exp_name='amip-p4K';
 glbmn_toa_cfmip_fb
 rad_ts_array_p4K=rad_ts_array;
 
-delR_p4K      =(mean(rad_ts_array_p4K(1,:))-mean(rad_ts_array_amip(1,:)))/4;
-del_clr_p4K   =(mean(rad_ts_array_p4K(2,:))-mean(rad_ts_array_amip(2,:)))/4;
-del_cre_p4K   =(mean(rad_ts_array_p4K(3,:))-mean(rad_ts_array_amip(3,:)))/4;
-del_lwcre_p4K =(mean(rad_ts_array_p4K(4,:))-mean(rad_ts_array_amip(4,:)))/4;
-del_swcre_p4K =(mean(rad_ts_array_p4K(5,:))-mean(rad_ts_array_amip(5,:)))/4;
-del_lwcs_p4K  =(mean(rad_ts_array_p4K(6,:))-mean(rad_ts_array_amip(6,:)))/4;
-del_swcs_p4K  =(mean(rad_ts_array_p4K(7,:))-mean(rad_ts_array_amip(7,:)))/4;
+delR_p4K      =(mean(rad_ts_array_p4K(1,:))-mean(rad_ts_array_amip(1,:)))/delT_amip_p4K;
+del_clr_p4K   =(mean(rad_ts_array_p4K(2,:))-mean(rad_ts_array_amip(2,:)))/delT_amip_p4K;
+del_cre_p4K   =(mean(rad_ts_array_p4K(3,:))-mean(rad_ts_array_amip(3,:)))/delT_amip_p4K;
+del_lwcre_p4K =(mean(rad_ts_array_p4K(4,:))-mean(rad_ts_array_amip(4,:)))/delT_amip_p4K;
+del_swcre_p4K =(mean(rad_ts_array_p4K(5,:))-mean(rad_ts_array_amip(5,:)))/delT_amip_p4K;
+del_lwcs_p4K  =(mean(rad_ts_array_p4K(6,:))-mean(rad_ts_array_amip(6,:)))/delT_amip_p4K;
+del_swcs_p4K  =(mean(rad_ts_array_p4K(7,:))-mean(rad_ts_array_amip(7,:)))/delT_amip_p4K;
 
 rad_fb_array(1,3)=delR_p4K;
 rad_fb_array(2,3)=del_clr_p4K;
@@ -126,14 +135,14 @@ rad_fb_array(5,3)=del_swcre_p4K;
 rad_fb_array(6,3)=del_lwcs_p4K;
 rad_fb_array(7,3)=del_swcs_p4K;
 
-%delR_p8K      =(mean(rad_ts_array_p4K(1,:))-mean(rad_ts_array_m4K(1,:)))/8;
-delR_p8K       =(mean(rad_ts_array_p4K(1,:))-mean(rad_ts_array_m4K(1,:)))/8;
-del_clr_p8K    =(mean(rad_ts_array_p4K(2,:))-mean(rad_ts_array_m4K(2,:)))/8;
-del_cre_p8K    =(mean(rad_ts_array_p4K(3,:))-mean(rad_ts_array_m4K(3,:)))/8;
-del_lwcre_p8K  =(mean(rad_ts_array_p4K(4,:))-mean(rad_ts_array_m4K(4,:)))/8;
-del_swcre_p8K  =(mean(rad_ts_array_p4K(5,:))-mean(rad_ts_array_m4K(5,:)))/8;
-del_lwcs_p8K   =(mean(rad_ts_array_p4K(6,:))-mean(rad_ts_array_m4K(6,:)))/8;
-del_swcs_p8K   =(mean(rad_ts_array_p4K(7,:))-mean(rad_ts_array_m4K(7,:)))/8;
+%delR_p8K      =(mean(rad_ts_array_p4K(1,:))-mean(rad_ts_array_m4K(1,:)))/delT_amip_8K;
+delR_p8K       =(mean(rad_ts_array_p4K(1,:))-mean(rad_ts_array_m4K(1,:)))/delT_amip_8K;
+del_clr_p8K    =(mean(rad_ts_array_p4K(2,:))-mean(rad_ts_array_m4K(2,:)))/delT_amip_8K;
+del_cre_p8K    =(mean(rad_ts_array_p4K(3,:))-mean(rad_ts_array_m4K(3,:)))/delT_amip_8K;
+del_lwcre_p8K  =(mean(rad_ts_array_p4K(4,:))-mean(rad_ts_array_m4K(4,:)))/delT_amip_8K;
+del_swcre_p8K  =(mean(rad_ts_array_p4K(5,:))-mean(rad_ts_array_m4K(5,:)))/delT_amip_8K;
+del_lwcs_p8K   =(mean(rad_ts_array_p4K(6,:))-mean(rad_ts_array_m4K(6,:)))/delT_amip_8K;
+del_swcs_p8K   =(mean(rad_ts_array_p4K(7,:))-mean(rad_ts_array_m4K(7,:)))/delT_amip_8K;
 
 rad_fb_array(1,4)=delR_p8K;
 rad_fb_array(2,4)=del_clr_p8K;
@@ -148,14 +157,14 @@ exp_name='amip-future4K';
 glbmn_toa_cfmip_fb
 rad_ts_array_future4K=rad_ts_array;
 
-;delR_future4K      =(mean(rad_ts_array_future4K(1,:))-mean(rad_ts_array_amip(1,:)))/4;
-delR_future4K      =(mean(rad_ts_array_future4K(1,:)-rad_ts_array_amip(1,:)))/4;
-del_clr_future4K   =(mean(rad_ts_array_future4K(2,:))-mean(rad_ts_array_amip(2,:)))/4;
-del_cre_future4K   =(mean(rad_ts_array_future4K(3,:))-mean(rad_ts_array_amip(3,:)))/4;
-del_lwcre_future4K =(mean(rad_ts_array_future4K(4,:))-mean(rad_ts_array_amip(4,:)))/4;
-del_swcre_future4K =(mean(rad_ts_array_future4K(5,:))-mean(rad_ts_array_amip(5,:)))/4;
-del_lwcs_future4K  =(mean(rad_ts_array_future4K(6,:))-mean(rad_ts_array_amip(6,:)))/4;
-del_swcs_future4K  =(mean(rad_ts_array_future4K(7,:))-mean(rad_ts_array_amip(7,:)))/4;
+delR_future4K      =(mean(rad_ts_array_future4K(1,:))-mean(rad_ts_array_amip(1,:)))/delT_amip_fut;
+;delR_future4K      =(mean(rad_ts_array_future4K(1,:)-rad_ts_array_amip(1,:)))/4;
+del_clr_future4K   =(mean(rad_ts_array_future4K(2,:))-mean(rad_ts_array_amip(2,:)))/delT_amip_fut;
+del_cre_future4K   =(mean(rad_ts_array_future4K(3,:))-mean(rad_ts_array_amip(3,:)))/delT_amip_fut;
+del_lwcre_future4K =(mean(rad_ts_array_future4K(4,:))-mean(rad_ts_array_amip(4,:)))/delT_amip_fut;
+del_swcre_future4K =(mean(rad_ts_array_future4K(5,:))-mean(rad_ts_array_amip(5,:)))/delT_amip_fut;
+del_lwcs_future4K  =(mean(rad_ts_array_future4K(6,:))-mean(rad_ts_array_amip(6,:)))/delT_amip_fut;
+del_swcs_future4K  =(mean(rad_ts_array_future4K(7,:))-mean(rad_ts_array_amip(7,:)))/delT_amip_fut;
 
 rad_fb_array(1,5)=delR_future4K
 rad_fb_array(2,5)=del_clr_future4K
@@ -174,7 +183,4 @@ rad_ts_array_4xCO2=rad_ts_array;
 
 delR_4xCO2=(mean(rad_ts_array_4xCO2(1,:))-mean(rad_ts_array_amip(1,:)))/1;
 
-
-
-
-
+%;
--- rcemip/rce_means.m
+++ rcemip/rce_means.m
@@ -4,36 +4,85 @@
 % variables with the _avg suffix indicate a domain mean has been taken         
 %
 % it looks like I still haven't downloaded the data for 295K pr in CAM5
+% an example of where the data is on glade: 
+% /glade/p/univ/ufsu0014/RCEMIP_CAM/CAM5_GCM/RCE_large295/0D/
 %
 % levi silvers                                                  Dec 2019       
 %-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 source='/Users/silvers/data/CAM/';
 
-model_generation='CAM5';
+mean_array=zeros(3,4);
+
+model_generation='CAM6';
 source_mod=strcat(source,model_generation,'_GCM_RCE/');
 
 var1='pr_avg';          
+var2='prw_avg';
+var3='rlut_avg';
+var4='wap500';
 
-% rad_ts_array=zeros(5,26281);                                                 
-                             
 % create string          
 SSTst="295";
 exp_string=strcat(model_generation,'_GCM_RCE_large',SSTst,'_0D_');
+exp_string_2D=strcat(model_generation,'_GCM_RCE_large',SSTst,'_2D_');
 s_var1=strcat(source_mod,exp_string,var1,'.nc') 
+s_var2=strcat(source_mod,exp_string,var2,'.nc') 
+s_var3=strcat(source_mod,exp_string,var3,'.nc') 
+s_var4=strcat(source_mod,exp_string_2D,var4,'.nc') 
 
 % read data
-pr_295    =ncread(s_var1,var1);
+pr_295      =ncread(s_var1,var1);
+prw_295     =ncread(s_var2,var2);
+rlut_295    =ncread(s_var3,var3);
+omega_295   =ncread(s_var4,var4);
 SSTst="300";
 exp_string=strcat(model_generation,'_GCM_RCE_large',SSTst,'_0D_');
 s_var1=strcat(source_mod,exp_string,var1,'.nc') 
-pr_300    =ncread(s_var1,var1);
+s_var2=strcat(source_mod,exp_string,var2,'.nc') 
+s_var3=strcat(source_mod,exp_string,var3,'.nc') 
+s_var4=strcat(source_mod,exp_string_2D,var4,'.nc') 
+pr_300      =ncread(s_var1,var1);
+prw_300     =ncread(s_var2,var2);
+rlut_300    =ncread(s_var3,var3);
+omega_300   =ncread(s_var4,var4);
 SSTst="305";
 exp_string=strcat(model_generation,'_GCM_RCE_large',SSTst,'_0D_');
 s_var1=strcat(source_mod,exp_string,var1,'.nc') 
-pr_305    =ncread(s_var1,var1);
+s_var2=strcat(source_mod,exp_string,var2,'.nc') 
+s_var3=strcat(source_mod,exp_string,var3,'.nc') 
+s_var4=strcat(source_mod,exp_string_2D,var4,'.nc') 
+pr_305      =ncread(s_var1,var1);
+prw_305     =ncread(s_var2,var2);
+rlut_305    =ncread(s_var3,var3);
+omega_305   =ncread(s_var4,var4);
 
 % scale precip to mm/day
-86400*mean(pr_295)
-86400*mean(pr_300)
-86400*mean(pr_305)
+p295=86400*mean(pr_295)
+p300=86400*mean(pr_300)
+p305=86400*mean(pr_305)
+
+pw295=mean(prw_295);
+pw300=mean(prw_300);
+pw305=mean(prw_305);
+olr295=mean(rlut_295);
+olr300=mean(rlut_300);
+olr305=mean(rlut_305);
+
+mean_array(1,1)=295;
+mean_array(2,1)=300;
+mean_array(3,1)=305;
+mean_array(1,2)=p295;
+mean_array(2,2)=p300;
+mean_array(3,2)=p305;
+mean_array(1,3)=pw295;
+mean_array(2,3)=pw300;
+mean_array(3,3)=pw305;
+mean_array(1,4)=olr295;
+mean_array(2,4)=olr300;
+mean_array(3,4)=olr305;
+
+
+
+
+
