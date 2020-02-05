fc18c72f9f21855a02d3ae704d1fea98591bdbc4
levi silvers <levi.silvers@stonybrook.edu>
2020-02-05 15:28:37 -0500

    added storage of lambda in array lambda() and added more comments to glbmn_toa_fb_driver.m

--- rcemip/glbmn_toa_fb_driver.m
+++ rcemip/glbmn_toa_fb_driver.m
@@ -1,10 +1,23 @@
+%-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+% computes the Cess type sensitivity for CAM5 and CAM6 RCE experiments
+% that all have a surface temperature change of 5K.
+% assuming a radiative forcing of -3.7 W/m2
+%
+% cess_eq -> stores the mean of the cess time series
+% cess_eq -> 1,2 are the sensitivities for CAM5
+% cess_eq -> 3,4 are the sensitivities for CAM6
+%
+% levi silvers                                           jan 2020
+%-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
 model_generation='CAM6';
 
 source='/Users/silvers/data/CAM/';
 
-ts_length=26257;
+ts_length=26257; % for the files with 26257 time steps, each step is one hour
 equilt   =2400; % time assumed to reach equilibrium 
 cess_eq=zeros(4,ts_length-equilt);
+lambda=zeros(4,ts_length-equilt); % del R/ del T
 
 source_mod=strcat(source,model_generation,'_GCM_RCE/');
 exp_string=strcat(model_generation,'_GCM_RCE_large295_0D_'); 
@@ -23,11 +36,15 @@ rad_305_array=rad_ts_array;
 cess_a=zeros(5,ts_length);
 cess_b=zeros(5,ts_length);
 cess_a=-(3.7*5)./(rad_300_array-rad_295_array);
+lambda_a=(rad_300_array-rad_295_array)/5;
 cess_b=-(3.7*5)./(rad_305_array-rad_300_array);
+lambda_b=(rad_305_array-rad_300_array)/5;
 
 % comppute the mean equilibrated(neglect 50 days) Cess sensitivity 
 cess_eq(3,:)=cess_a(1,equilt+1:ts_length);
 cess_eq(4,:)=cess_b(1,equilt+1:ts_length);
+lambda(3,:)=lambda_a(1,equilt+1:ts_length);
+lambda(4,:)=lambda_b(1,equilt+1:ts_length);
 
 figure
 subplot(3,1,1)
@@ -79,15 +96,20 @@ rad_305_array=rad_ts_array;
 cess_a=zeros(5,ts_length);
 cess_b=zeros(5,ts_length);
 cess_a=-(3.7*5)./(rad_300_array-rad_295_array);
+lambda_a=(rad_300_array-rad_295_array)/5;
 cess_b=-(3.7*5)./(rad_305_array-rad_300_array);
+lambda_b=(rad_305_array-rad_300_array)/5;
 
-% comppute the mean equilibrated(neglect 50 days) Cess sensitivity 
+% comppute the mean equilibrated(neglect 100 days) Cess sensitivity 
 cess_eq(1,:)=cess_a(1,equilt+1:ts_length);
 cess_eq(2,:)=cess_b(1,equilt+1:ts_length);
+lambda(1,:)=lambda_a(1,equilt+1:ts_length);
+lambda(2,:)=lambda_b(1,equilt+1:ts_length);
 
-cess_feedback(1,:)=(rad_305_array-rad_300_array)./5.;
+%cess_feedback(1,:)=(rad_305_array-rad_300_array)./5.;
 
 mean_cess=mean(cess_eq,2);
+mean_lambda=mean(lambda,2);
 
 figure
 subplot(3,1,1)
