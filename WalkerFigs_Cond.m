28401db553fadeb5286bc842ae17f523d5369609
levi silvers <levi.silvers@stonybrook.edu>
2020-09-21 11:20:01 -0400

    changes to WalkerFigs_Cond.m and Walker_readfrom_source.m to make a two panel plot shownig the total condensate, and the specific humidity for Silvers and Robinson, 2020.

--- WalkerFigs_Cond.m
+++ WalkerFigs_Cond.m
@@ -85,6 +85,92 @@ cond_25km_lg_prof_lwoff=ice_25km_lg_lwoff_prof+liq_25km_lg_lwoff_prof;
 
 %===========
 
+
+lcl_25km_lg=ncread(source_25km_lg_month,'low_cld_amt');
+lcl_25km_lg_lwoff=ncread(source_25km_lg_month_lwoff,'low_cld_amt');
+lcl_25km_sm=ncread(source_gcm_ctl,'low_cld_amt');
+lcl_25km_sm_lwoff=ncread(source_gcm_lwoff,'low_cld_amt');
+lcl_E25km = ncread(source_gcm_noconv,'low_cld_amt');
+lcl_E25km_lwoff = ncread(source_gcm_noconv_lwoff,'low_cld_amt');
+
+% average in space
+lcl_E25km_a=squeeze(mean(lcl_E25km,1));
+lcl_E25km_b=squeeze(mean(lcl_E25km_a,1));
+lcl_E25km_c=squeeze(mean(lcl_E25km_b,1));
+
+lcl_E25km_a=squeeze(mean(lcl_E25km_lwoff,1));
+lcl_E25km_b=squeeze(mean(lcl_E25km_a,1));
+lcl_E25km_lwoff_c=squeeze(mean(lcl_E25km_b,1));
+
+lcl_25km_lg_a=squeeze(mean(lcl_25km_lg,1));
+lcl_25km_lg_b=squeeze(mean(lcl_25km_lg_a,1));
+lcl_25km_lg_c=squeeze(mean(lcl_25km_lg_b,1));
+
+lcl_25km_lg_lwoff_a=squeeze(mean(lcl_25km_lg_lwoff,1));
+lcl_25km_lg_lwoff_b=squeeze(mean(lcl_25km_lg_lwoff_a,1));
+lcl_25km_lg_lwoff_c=squeeze(mean(lcl_25km_lg_lwoff_b,1));
+
+lcl_25km_sm_a=squeeze(mean(lcl_25km_sm,1));
+lcl_25km_sm_b=squeeze(mean(lcl_25km_sm_a,1));
+lcl_25km_sm_c=squeeze(mean(lcl_25km_sm_b,1));
+
+lcl_25km_sm_lwoff_a=squeeze(mean(lcl_25km_sm_lwoff,1));
+lcl_25km_sm_lwoff_b=squeeze(mean(lcl_25km_sm_lwoff_a,1));
+lcl_25km_sm_lwoff_c=squeeze(mean(lcl_25km_sm_lwoff_b,1));
+
+%
+lcl_100km_lg=ncread(source_100km_lg_month,'low_cld_amt');
+lcl_100km_lg_lwoff=ncread(source_100km_lg_month_lwoff,'low_cld_amt');
+lcl_100km_sm=ncread(source_100km_sm_month,'low_cld_amt');
+lcl_100km_sm_lwoff=ncread(source_100km_sm_month_lwoff,'low_cld_amt');
+
+% average in space
+lcl_100km_lg_a=squeeze(mean(lcl_100km_lg,1));
+lcl_100km_lg_b=squeeze(mean(lcl_100km_lg_a,1));
+lcl_100km_lg_c=squeeze(mean(lcl_100km_lg_b,1));
+
+lcl_100km_lg_lwoff_a=squeeze(mean(lcl_100km_lg_lwoff,1));
+lcl_100km_lg_lwoff_b=squeeze(mean(lcl_100km_lg_lwoff_a,1));
+lcl_100km_lg_lwoff_c=squeeze(mean(lcl_100km_lg_lwoff_b,1));
+
+lcl_100km_sm_a=squeeze(mean(lcl_100km_sm,1));
+lcl_100km_sm_b=squeeze(mean(lcl_100km_sm_a,1));
+lcl_100km_sm_c=squeeze(mean(lcl_100km_sm_b,1));
+
+lcl_100km_sm_lwoff_a=squeeze(mean(lcl_100km_sm_lwoff,1));
+lcl_100km_sm_lwoff_b=squeeze(mean(lcl_100km_sm_lwoff_a,1));
+lcl_100km_sm_lwoff_c=squeeze(mean(lcl_100km_sm_lwoff_b,1));
+
+
+
+%===========
+
+q_1km_ztmn_prof       =squeeze(mean(q_1km_ztmn,1));
+q_2km_ztmn_prof       =squeeze(mean(q_2km_ztmn,1));
+q_25km_ztmn_prof      =squeeze(mean(q_25km_ztmn,1));
+q_E25km_ztmn_prof     =squeeze(mean(q_E25km_ztmn,1));
+q_100km_ztmn_prof     =squeeze(mean(q_100km_ztmn,1));
+q_100km_lg_ztmn_prof  =squeeze(mean(q_100km_lg_ztmn,1));
+
+q_1km_lwoff_ztmn_prof       =squeeze(mean(q_1km_lwoff_ztmn,1));
+q_2km_lwoff_ztmn_prof       =squeeze(mean(q_2km_lwoff_ztmn,1));
+q_25km_lwoff_ztmn_prof      =squeeze(mean(q_25km_lwoff_ztmn,1));
+q_E25km_lwoff_ztmn_prof     =squeeze(mean(q_E25km_lwoff_ztmn,1));
+
+q_100km_bias    =q_100km_ztmn_prof-q_1km_ztmn_prof;
+q_100km_lg_bias =q_100km_lg_ztmn_prof-q_1km_ztmn_prof;
+q_25km_bias     =q_25km_ztmn_prof-q_1km_ztmn_prof;
+q_E25km_bias    =q_E25km_ztmn_prof-q_1km_ztmn_prof;
+q_2km_bias      =q_2km_ztmn_prof-q_1km_ztmn_prof;
+q_1km_bias      =q_1km_ztmn_prof-q_1km_ztmn_prof;
+
+q_25km_lwoff_bias     =q_25km_lwoff_ztmn_prof-q_1km_ztmn_prof;
+q_E25km_lwoff_bias    =q_E25km_lwoff_ztmn_prof-q_1km_ztmn_prof;
+q_2km_lwoff_bias      =q_2km_lwoff_ztmn_prof-q_1km_ztmn_prof;
+q_1km_lwoff_bias      =q_1km_lwoff_ztmn_prof-q_1km_ztmn_prof;
+
+%===========
+
 liq_25km_ztmn=read_1var_ztmn(source_gcm_ctl,'tot_liq_amt');
 liq_25km_prof=squeeze(mean(liq_25km_ztmn,1));
 ice_25km_ztmn=read_1var_ztmn(source_gcm_ctl,'tot_ice_amt');
@@ -93,25 +179,25 @@ ice_25km_prof=squeeze(mean(ice_25km_ztmn,1));
 cond_25km_prof_ctl=ice_25km_prof+liq_25km_prof;
 
 liq_25km_ztmn=read_1var_ztmn(source_gcm_noconv,'tot_liq_amt');
-liq_25km_prof=squeeze(mean(liq_25km_ztmn,1));
+liq_E25km_prof=squeeze(mean(liq_25km_ztmn,1));
 ice_25km_ztmn=read_1var_ztmn(source_gcm_noconv,'tot_ice_amt');
-ice_25km_prof=squeeze(mean(ice_25km_ztmn,1));
+ice_E25km_prof=squeeze(mean(ice_25km_ztmn,1));
 
-cond_25km_prof_noconv=ice_25km_prof+liq_25km_prof;
+cond_E25km_prof=ice_E25km_prof+liq_E25km_prof;
 
-liq_25km_ztmn=read_1var_ztmn(source_gcm_lwoff,'tot_liq_amt');
-liq_25km_prof=squeeze(mean(liq_25km_ztmn,1));
-ice_25km_ztmn=read_1var_ztmn(source_gcm_lwoff,'tot_ice_amt');
-ice_25km_prof=squeeze(mean(ice_25km_ztmn,1));
+liq_25km_lwoff_ztmn=read_1var_ztmn(source_gcm_lwoff,'tot_liq_amt');
+liq_25km_prof_lwoff=squeeze(mean(liq_25km_lwoff_ztmn,1));
+ice_25km_lwoff_ztmn=read_1var_ztmn(source_gcm_lwoff,'tot_ice_amt');
+ice_25km_prof_lwoff=squeeze(mean(ice_25km_lwoff_ztmn,1));
 
-cond_25km_prof_lwoff=ice_25km_prof+liq_25km_prof;
+cond_25km_prof_lwoff=ice_25km_prof_lwoff+liq_25km_prof_lwoff;
 
 liq_25km_ztmn=read_1var_ztmn(source_gcm_noconv_lwoff,'tot_liq_amt');
-liq_25km_prof=squeeze(mean(liq_25km_ztmn,1));
+liq_E25km_prof_lwoff=squeeze(mean(liq_25km_ztmn,1));
 ice_25km_ztmn=read_1var_ztmn(source_gcm_noconv_lwoff,'tot_ice_amt');
-ice_25km_prof=squeeze(mean(ice_25km_ztmn,1));
+ice_E25km_prof_lwoff=squeeze(mean(ice_25km_ztmn,1));
 
-cond_25km_prof_noconv_lwoff=ice_25km_prof+liq_25km_prof;
+cond_E25km_prof_lwoff=ice_E25km_prof_lwoff+liq_E25km_prof_lwoff;
 
 liq_2km_ztmn=read_1var_ztmn(source_2km_month,'tot_liq_amt');
 liq_2km_prof=squeeze(mean(liq_2km_ztmn,1));
@@ -120,12 +206,12 @@ ice_2km_prof=squeeze(mean(ice_2km_ztmn,1));
 
 cond_2km_prof_ctl=ice_2km_prof+liq_2km_prof;
 
-liq_2km_ztmn=read_1var_ztmn(source_2km_month_lwoff,'tot_liq_amt');
-liq_2km_prof=squeeze(mean(liq_2km_ztmn,1));
-ice_2km_ztmn=read_1var_ztmn(source_2km_month_lwoff,'tot_ice_amt');
-ice_2km_prof=squeeze(mean(ice_2km_ztmn,1));
+liq_2km_lwoff_ztmn=read_1var_ztmn(source_2km_month_lwoff,'tot_liq_amt');
+liq_2km_prof_lwoff=squeeze(mean(liq_2km_lwoff_ztmn,1));
+ice_2km_lwoff_ztmn=read_1var_ztmn(source_2km_month_lwoff,'tot_ice_amt');
+ice_2km_prof_lwoff=squeeze(mean(ice_2km_lwoff_ztmn,1));
 
-cond_2km_prof_lwoff=ice_2km_prof+liq_2km_prof;
+cond_2km_prof_lwoff=ice_2km_prof_lwoff+liq_2km_prof_lwoff;
 
 liq_1km_ztmn=read_1var_ztmn(source_1km_month,'tot_liq_amt');
 liq_1km_prof=squeeze(mean(liq_1km_ztmn,1));
@@ -134,12 +220,12 @@ ice_1km_prof=squeeze(mean(ice_1km_ztmn,1));
 
 cond_1km_prof_ctl=ice_1km_prof+liq_1km_prof;
 
-liq_1km_ztmn=read_1var_ztmn(source_1km_month_lwoff,'tot_liq_amt');
-liq_1km_prof=squeeze(mean(liq_1km_ztmn,1));
-ice_1km_ztmn=read_1var_ztmn(source_1km_month_lwoff,'tot_ice_amt');
-ice_1km_prof=squeeze(mean(ice_1km_ztmn,1));
+liq_1km_lwoff_ztmn=read_1var_ztmn(source_1km_month_lwoff,'tot_liq_amt');
+liq_1km_prof_lwoff=squeeze(mean(liq_1km_lwoff_ztmn,1));
+ice_1km_lwoff_ztmn=read_1var_ztmn(source_1km_month_lwoff,'tot_ice_amt');
+ice_1km_prof_lwoff=squeeze(mean(ice_1km_lwoff_ztmn,1));
 
-cond_1km_prof_lwoff=ice_1km_prof+liq_1km_prof;
+cond_1km_prof_lwoff=ice_1km_prof_lwoff+liq_1km_prof_lwoff;
 
 % total zonal time mean condensate
 liq_25km_tot_ztmn=liq_25km_ztmn+ice_25km_ztmn;
@@ -149,6 +235,7 @@ scfac=1000.
 
     %'YTickLabel',{'100','200','300','400','500','600','700','800','900','1000'},...
 figure_condensation=figure
+%subfigure(1,2,1)
 axes2 = axes('Parent',figure_condensation,'BoxStyle','full','YMinorTick','on',...
     'YTickLabel',{'100','200','','400','','600','','800','','1000'},...
     'YScale','log',...
@@ -169,9 +256,47 @@ hold on
 plot(scfac.*cond_1km_prof_lwoff,pfull_2km,'--','Color',colgrn,'LineWidth',2)
 plot(scfac.*cond_2km_prof_ctl,pfull_2km,'Color',colblu,'LineWidth',2)
 plot(scfac.*cond_2km_prof_lwoff,pfull_2km,'--','Color',colblu,'LineWidth',2)
-plot(scfac.*cond_25km_prof_noconv,pfull_2km,'Color',colpur,'LineWidth',2)
-plot(scfac.*cond_25km_prof_noconv_lwoff,pfull_2km,'--','Color',colpur,'LineWidth',2)
+plot(scfac.*cond_E25km_prof,pfull_2km,'Color',colpur,'LineWidth',2)
+plot(scfac.*cond_E25km_prof_lwoff,pfull_2km,'--','Color',colpur,'LineWidth',2)
 plot(scfac.*cond_25km_prof_ctl,pfull_2km,'Color',colyel,'LineWidth',2)
+plot(scfac.*cond_25km_prof_lwoff,pfull_2km,'--','Color',colyel,'LineWidth',2)
+title('Domain Mean Liq + Ice ')
+xlabel('g/kg')
+ylabel('presssure (hPa)')
+
+% plot ice and liq individually
+figure_condensation=figure
+axes2 = axes('Parent',figure_condensation,'BoxStyle','full','YMinorTick','on',...
+    'YTickLabel',{'100','200','','400','','600','','800','','1000'},...
+    'YScale','log',...
+    'YTick',[10000 20000 30000 40000 50000 60000 70000 80000 90000 100000],...
+    'Layer','top',...
+    'YDir','reverse',...
+    'FontWeight','bold',...
+    'FontSize',16);%,...
+% apparently the position of the plot command makes a big difference
+ylim(axes2,[10000 100000]);
+box(axes2,'on');
+hold(axes2,'on');
+plot(scfac.*cond_1km_prof_ctl,pfull_2km,'Color',colgrn,'LineWidth',2)
+set(gca,'Ydir','reverse')
+set(gca,'YScale','log')
+hold on
+set(gca,'Ydir','reverse')
+plot(scfac.*ice_1km_prof,pfull_2km,'Color',colgrn,'LineWidth',1)
+plot(scfac.*liq_1km_prof,pfull_2km,'Color',colgrn,'LineWidth',1)
+plot(scfac.*cond_1km_prof_lwoff,pfull_2km,'--','Color',colgrn,'LineWidth',2)
+plot(scfac.*cond_2km_prof_ctl,pfull_2km,'Color',colblu,'LineWidth',2)
+plot(scfac.*ice_2km_prof,pfull_2km,'Color',colblu,'LineWidth',1)
+plot(scfac.*liq_2km_prof,pfull_2km,'Color',colblu,'LineWidth',1)
+plot(scfac.*cond_2km_prof_lwoff,pfull_2km,'--','Color',colblu,'LineWidth',2)
+plot(scfac.*cond_E25km_prof,pfull_2km,'Color',colpur,'LineWidth',2)
+plot(scfac.*ice_E25km_prof,pfull_2km,'Color',colpur,'LineWidth',1)
+plot(scfac.*liq_E25km_prof,pfull_2km,'Color',colpur,'LineWidth',1)
+plot(scfac.*cond_E25km_prof_lwoff,pfull_2km,'--','Color',colpur,'LineWidth',2)
+plot(scfac.*cond_25km_prof_ctl,pfull_2km,'Color',colyel,'LineWidth',2)
+plot(scfac.*ice_25km_prof,pfull_2km,'Color',colyel,'LineWidth',1)
+plot(scfac.*liq_25km_prof,pfull_2km,'Color',colyel,'LineWidth',1)
 plot(scfac.*cond_25km_prof_lwoff,pfull_2km,'--','Color',colyel,'LineWidth',2)
 title('Domain Mean Liq + Ice ')
 xlabel('g/kg')
@@ -325,5 +450,87 @@ xlabel('g/kg')
 ylabel('presssure (hPa)')
 
 
+
+figure_25km_q=figure
+axes2 = axes('Parent',figure_25km_q,'BoxStyle','full','YMinorTick','on',...
+    'YTickLabel',{'100','200','','400','','600','','800','','1000'},...
+    'YScale','log',...
+    'XScale','log',...
+    'YTick',[10000 20000 30000 40000 50000 60000 70000 80000 90000 100000],...
+    'Layer','top',...
+    'YDir','reverse',...
+    'FontWeight','bold',...
+    'FontSize',16);%,...
+% apparently the position of the plot command makes a big difference
+    %plot(cond_1km_prof_ctl,pfull_2km,'Color',colgrn,'LineWidth',2)
+ylim(axes2,[10000 100000]);
+box(axes2,'on');
+hold(axes2,'on');
+plot(scfac.*q_25km_ztmn_prof,pfull_2km,'Color',colyel,'LineWidth',2)
+set(gca,'Ydir','reverse')
+set(gca,'YScale','log')
+hold on
+%plot(scfac.*q_25km_ztmn_prof,pfull_2km,'--','Color',colyel,'LineWidth',2)
+plot(scfac.*q_E25km_ztmn_prof,pfull_2km,'Color',colpur,'LineWidth',3)
+plot(scfac.*q_2km_ztmn_prof,pfull_2km,'Color',colblu,'LineWidth',3)
+plot(scfac.*q_1km_ztmn_prof,pfull_2km,'Color',colgrn,'LineWidth',3)
+%plot(scfac.*cond_25km_lg_prof_lwoff,pfull_2km,'--','Color',colyel,'LineWidth',3)
+%plot(scfac.*cond_100km_prof_ctl,pfull_2km,'red','LineWidth',2)
+%plot(scfac.*cond_100km_prof_lwoff,pfull_2km,'--','Color','red','LineWidth',2)
+%plot(scfac.*cond_100km_lg_prof_ctl,pfull_2km,'Color','red','LineWidth',3)
+%plot(scfac.*cond_100km_lg_prof_lwoff,pfull_2km,'--','Color','red','LineWidth',3)
+title('Domain Mean q ')
+xlabel('g/kg')
+ylabel('presssure (hPa)')
+
+% Two panel figure showing today domain mean liq+ice and domain mean 
+% total condensate (liquid + ice) and the specific humidity (q)
+figure_condensation=figure
+subplot(1,2,1)
+plot(scfac.*cond_1km_prof_ctl,pfull_2km,'Color',colgrn,'LineWidth',2)
+ax1=gca;
+set(ax1,'Ydir','reverse')
+set(ax1,'YScale','log')
+ylim(ax1,[10000 100000]);
+ax1.YTick = [10000 20000 30000 40000 50000 60000 70000 80000 90000 100000]
+ax1.YTickLabels = [100 200 300 " " 500 " " 700 " " 900 1000]
+ax1.FontSize = 14;
+ax1.FontWeight = 'bold';
+hold on
+plot(scfac.*cond_1km_prof_lwoff,pfull_2km,'--','Color',colgrn,'LineWidth',2)
+plot(scfac.*cond_2km_prof_ctl,pfull_2km,'Color',colblu,'LineWidth',2)
+plot(scfac.*cond_2km_prof_lwoff,pfull_2km,'--','Color',colblu,'LineWidth',2)
+plot(scfac.*cond_E25km_prof,pfull_2km,'Color',colpur,'LineWidth',2)
+plot(scfac.*cond_E25km_prof_lwoff,pfull_2km,'--','Color',colpur,'LineWidth',2)
+plot(scfac.*cond_25km_prof_ctl,pfull_2km,'Color',colyel,'LineWidth',2)
+plot(scfac.*cond_25km_prof_lwoff,pfull_2km,'--','Color',colyel,'LineWidth',2)
+title('Total Condensate')
+xlabel('g/kg')
+ylabel('presssure (hPa)')
+
+%figure_25km_q=figure
+subplot(1,2,2)
+plot(scfac.*q_25km_bias,pfull_2km,'Color',colyel,'LineWidth',2)
+ax2=gca;
+set(ax2,'Ydir','reverse')
+set(ax2,'YScale','log')
+ylim(ax2,[10000 100000]);
+ax2.YTick = [10000 20000 30000 40000 50000 60000 70000 80000 90000 100000]
+ax2.YTickLabels = [" " " " " " " " " " " " " " " " " " " "]
+ax2.FontSize = 14;
+ax2.FontWeight = 'bold';
+hold on
+%plot(scfac.*q_25km_lwoff_bias,pfull_2km,'--','Color',colyel,'LineWidth',2)
+plot(scfac.*q_E25km_bias,pfull_2km,'Color',colpur,'LineWidth',2)
+%plot(scfac.*q_E25km_lwoff_bias,pfull_2km,'--','Color',colpur,'LineWidth',2)
+%plot(scfac.*q_100km_bias,pfull_2km,'r','LineWidth',2)
+%plot(scfac.*q_100km_lg_bias,pfull_2km,'--r','LineWidth',2)
+plot(scfac.*q_2km_bias,pfull_2km,'Color',colblu,'LineWidth',2)
+%plot(scfac.*q_2km_lwoff_bias,pfull_2km,'--','Color',colblu,'LineWidth',2)
+plot(scfac.*q_1km_bias,pfull_2km,'Color',colgrn,'LineWidth',2)
+%plot(scfac.*q_1km_lwoff_bias,pfull_2km,'--','Color',colgrn,'LineWidth',2)
+title('Specific Humidity Bias')
+xlabel('g/kg')
+%ylabel('presssure (hPa)')
 
 
--- Walker_readfrom_source.m
+++ Walker_readfrom_source.m
@@ -10,6 +10,19 @@
 %source_1km_month=strcat(path_n,'c10x4000L33_am4p0_1km_wlkr_4K_1979_6mn.atmos_month.nc');
 %source_2km_month=strcat(path_n,'c50x2000L33_am4p0_2km_wlkr_4K_1979_6mn.atmos_month.nc');
 %
+source_gcm_month             =strcat('/Users/silvers/data/WalkerCell/testing_20181203/c8x160L33_am4p0_25km_wlkr_ent0p9/',yearstr,'.atmos_month_tmn.nc');
+source_gcm_month_noconv      =strcat('/Users/silvers/data/WalkerCell/testing_20181203/c8x160L33_am4p0_25km_wlkr_ent0p9_noconv/',yearstr,'.atmos_month_tmn.nc');
+source_gcm_month_noconv_lwoff=strcat('/Users/silvers/data/WalkerCell/testing_20181203/c8x160L33_am4p0_25km_wlkr_ent0p9_noconv_lwoff/',yearstr,'.atmos_month_tmn.nc');
+source_gcm_month_lwoff       =strcat('/Users/silvers/data/WalkerCell/testing_20181203/c8x160L33_am4p0_25km_wlkr_ent0p9_lwoff/',yearstr,'.atmos_month_tmn.nc');
+
+lwstring='4K/';
+source_1km_month=strcat(path_n,'c10x4000L33_am4p0_1km_wlkr_',lwstring,'1979_6mn.atmos_month.nc');
+source_2km_month=strcat(path_n,'c50x2000L33_am4p0_2km_wlkr_',lwstring,'1979_6mn.atmos_month.nc');
+lwstring='4K_lwoff/';
+source_1km_month_lwoff=strcat(path_n,'c10x4000L33_am4p0_1km_wlkr_',lwstring,'1979_6mn.atmos_month.nc');
+source_2km_month_lwoff=strcat(path_n,'c50x2000L33_am4p0_2km_wlkr_',lwstring,'1979_6mn.atmos_month.nc');
+
+
 
 temp_eq_E25_ztmn=read_1var_ztmn(source_gcm_month_noconv,'temp');
 temp_eq_ztmn=read_1var_ztmn(source_gcm_month,'temp');
@@ -18,14 +31,30 @@ temp_crm1_ztmn=read_1var_ztmn(source_1km_month,'temp');
 
 temp_25km_ztmn = read_1var_ztmn(source_gcm_month,'temp');
 
+q_100km_lg_ztmn    = read_1var_ztmn(source_100km_lg_month,'sphum');
+q_100km_ztmn   = read_1var_ztmn(source_100km_sm_month,'sphum');
 q_25km_ztmn    = read_1var_ztmn(source_gcm_month,'sphum');
 q_E25km_ztmn    = read_1var_ztmn(source_gcm_month_noconv,'sphum');
+q_25km_lwoff_ztmn    = read_1var_ztmn(source_gcm_month_lwoff,'sphum');
+q_E25km_lwoff_ztmn    = read_1var_ztmn(source_gcm_month_noconv_lwoff,'sphum');
 
 q_2km=ncread(source_2km_month,'sphum');
 q_2km_zmn=squeeze(mean(q_2km,2));
-q_2km_ztmn=squeeze(q_2km_zmn(:,:,1));
+q_2km_ztmn=squeeze(mean(q_2km_zmn,3));
+
+q_2km_lwoff=ncread(source_2km_month_lwoff,'sphum');
+q_2km_lwoff_zmn=squeeze(mean(q_2km_lwoff,2));
+q_2km_lwoff_ztmn=squeeze(mean(q_2km_lwoff_zmn,3));
 
 q_1km=ncread(source_1km_month,'sphum');
 q_1km_zmn=squeeze(mean(q_1km,2));
-q_1km_ztmn=squeeze(q_1km_zmn(:,:,1));
+q_1km_ztmn=squeeze(mean(q_1km_zmn,3));
+
+q_1km_lwoff=ncread(source_1km_month_lwoff,'sphum');
+q_1km_lwoff_zmn=squeeze(mean(q_1km_lwoff,2));
+q_1km_lwoff_ztmn=squeeze(mean(q_1km_lwoff_zmn,3));
+
+
+
+
 
