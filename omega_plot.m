401b769c89eec872e98d5011ddf352584be82fd9
levi silvers <levi.silvers@stonybrook.edu>
2022-01-27 15:29:38 -0500

    commit updates to the poisson.m solver script and add a script that plots results from poisson.m

---
+++ omega_plot.m
@@ -0,0 +1,453 @@
+%--------------------------------------------------------
+% plots figures that are the result of running poisson.m
+%
+% levi silvers                               jan 2022
+%--------------------------------------------------------
+
+omega_lon_sch=omega_lon(lon1:lon2,:);
+
+walker             =[0.984313725490196, 0.501960784313725, 0.447058823529412];  
+hadley             =[0.807843137254902, 0.427450980392157, 0.741176470588235];
+totalc             =[0.549019607843137, 0.427450980392157, 0.192156862745098];  
+
+%function cont_map_modis(field_in,vlat,vlon,contsin,caxisin)
+%load coast 
+%figure;
+%axesm('MapProjection','hammer','origin',[0,0])               
+%framem 
+
+%load coast
+%axesm('MapProjection','hammer','origin',[0,0])
+%framem
+
+cmap_blue=[255,255,255;                                      
+197,255,255;                                                 
+138,255,255;                                                 
+82,211,255;                                                  
+27,153,255;                                                  
+0,96,226;
+0,41,167;
+0,0,110;
+0,0,55;
+0,0,0];
+%cmap_orang=[255,255,255;                                    
+%255,255,197;                                                
+%255,255,138;
+%255,211,82;
+%255,153,27;
+%226,96,0;
+%168,41,0;
+%111,0,0;
+%56,0,0;
+%0,0,0];
+cmap_dirtorange=[255,255,255;
+255,255,200;
+255,240,175;
+255,225,150;
+255,210,125;
+255,195,100;
+235,180,75;
+205,165,50;
+185,133,25;
+165,91,10];
+%cmap_blueorange=[10,41,255;
+%10,100,255;
+%27,153,255;
+%138,255,255;
+%225,255,255;
+%255,255,225;
+%255,255,138;
+%255,153,27;
+%255,100,10;
+%255,41,10];
+cmap_blueorange=[10,50,255;
+10,100,255;
+10,150,255;
+27,200,255;
+138,250,255;
+%225,255,255; % these are very light
+%255,255,225;
+255,250,138;
+255,200,27;
+255,150,10;
+255,100,10;
+255,50,10];
+cmap=cmap_blueorange/256;
+%colormap(cmap(1:10,:))
+%plotm(latitude,longtude,'k')
+%gridm
+
+
+%conts=[-80,-70,-60,-50,-40,-30,-20,-10,0,10,20,30,40,50,60,70,80]; % hPa/day
+conts=[-80,-65,-50,-35,-20,-5,5,20,35,50,65,80]; % hPa/day
+
+load coastlines
+Lthick=1;
+longitude=longtude-177.5;
+
+figure
+%subplot(3,1,1)
+%contourfm(latitude,longtude,squeeze(omega_total'),conts)
+caxis=([-80,80]);
+contourfm(latitude,longitude,squeeze(omega_tot_678'),conts)
+title('Total Overturning motion')
+colorbar
+cmap=cmap_blueorange/256;
+colormap(cmap(1:10,:))
+
+%ylabel('latitude')
+
+figure
+%subplot(3,1,2)
+%contourf(omega_lon',conts,caxis)
+contourfm(latitude,longtude,squeeze(omega_wc'),conts)
+title('Overturning motion in the zonal direction: local WC')
+colorbar
+ylabel('latitude')
+figure
+%subplot(3,1,3)
+%contourf(omega_lat',conts,caxis)
+contourfm(latitude,longtude,squeeze(omega_hc'),conts)
+title('Overturning motion in the meridional direction: local HC')
+colorbar
+ylabel('latitude')
+xlabel('longitude')
+
+omega_loc_tot_zm=mean(omega_total,1);
+omega_loc_lon_zm=mean(omega_wc,1);
+omega_loc_lat_zm=mean(omega_hc,1);
+omega_loc_tot_mm=mean(omega_total,2);
+omega_loc_lon_mm=mean(omega_wc,2);
+omega_loc_lat_mm=mean(omega_hc,2);
+
+figure
+subplot(2,1,1)
+plot(latitude,omega_loc_tot_zm,'color',totalc)
+hold on
+plot(latitude,omega_loc_lon_zm,'color',walker)
+plot(latitude,omega_loc_lat_zm,'color',hadley)
+xlim([-90 90])
+ylabel('hPa/day')
+xlabel('latitude')
+title('Zonal Mean of Local Circulations')
+%figure
+subplot(2,1,2)
+plot(longtude,omega_loc_tot_mm,'color',totalc)
+hold on
+plot(longtude,omega_loc_lon_mm,'color',walker)
+plot(longtude,omega_loc_lat_mm,'color',hadley)
+xlim([1 360])
+ylabel('hPa/day')
+xlabel('longitude')
+title('Meridional Mean of Local Circulations')
+
+% this creates pdf's of the whole domain, including more than the tropics
+% the values near zero tend to dominate in this case.  
+figure
+bwide=5;
+h1=histogram(omega_total);
+h1.Normalization = 'probability';
+h1.BinWidth = bwide;
+hold on
+h2=histogram(omega_hc);
+h2.BinWidth = bwide;
+h2.Normalization = 'probability';
+h3=histogram(omega_wc);
+h3.Normalization = 'probability';
+h3.BinWidth = bwide;
+
+%numberb=100;
+%xlim1=-80;
+%bwide=1;
+%figure
+%subplot(3,1,1)
+%h1=histogram(omega_tot);
+%h1.Normalization = 'probability';
+%h1.BinWidth = bwide;
+%%h1.NumBins = numberb;
+%xlim([xlim1 80])
+%subplot(3,1,2)
+%h2=histogram(omega_lat);
+%h2.Normalization = 'probability';
+%h2.BinWidth = bwide;
+%%h2.NumBins = numberb;
+%xlim([xlim1 80])
+%subplot(3,1,3)
+%h3=histogram(omega_lon);
+%h3.Normalization = 'probability';
+%h3.BinWidth = bwide;
+%%h3.NumBins = numberb;
+%xlim([xlim1 80])
+
+%------------------------------------------
+% grab the regional WC used in Schwendike et al., 2014
+% in CESM, 30:51 correspond to +5, -35
+%
+% grab the area (5N:35S) of interest
+lat1=30;
+lat2=51;
+% grab a more traditional definition of tropics: 
+lat_tr1=33;
+lat_tr2=64;
+% grab the area (110E:160E) of interest for the regional HC
+lon1=45;
+lon2=65;
+%------------------------------------------
+
+%------------------------------------------
+% solve the poisson equation at several timesteps
+%------------------------------------------
+% Schwendike et al. 2014 look at DJF and JJA, that could be a good starting point
+
+clear omega_lon_t;
+clear omega_lat_t;
+clear omega_tot_t;
+
+% solve the poisson equation at several timesteps
+%timelevel=12; % --> timelevel is used in poisson.m
+timelevelend=42; % should grab values from 3months each year for 3 years
+specialindex=1;
+%for timeindex=1:12:timelevelend
+%  timelevel=timeindex
+%  timelevel=timelevel+1
+%  timelevel=timelevel+1
+%%  specialindex=specialindex+1
+%  %
+%  %timelevel=timeindex+1
+%%  specialindex=specialindex+1
+%  %  
+%  %timelevel=timeindex+2
+%%  specialindex=specialindex+1
+%end
+for timeindex=6:12:timelevelend
+  timelevel = timeindex;
+  poisson % calls the poisson solver
+  omega_lon_t(:,:,specialindex)=omega_wc;
+  omega_lat_t(:,:,specialindex)=omega_hc;
+  omega_tot_t(:,:,specialindex)=omega_total;
+  %
+  timelevel=timelevel+1;
+  specialindex=specialindex+1;
+  poisson % calls the poisson solver
+  omega_lon_t(:,:,specialindex)=omega_wc;
+  omega_lat_t(:,:,specialindex)=omega_hc;
+  omega_tot_t(:,:,specialindex)=omega_total;
+  %
+  timelevel=timelevel+1;
+  specialindex=specialindex+1;
+  poisson % calls the poisson solver
+  omega_lon_t(:,:,specialindex)=omega_wc;
+  omega_lat_t(:,:,specialindex)=omega_hc;
+  omega_tot_t(:,:,specialindex)=omega_total;
+  %timelevel=timelevel+12
+  %timelevel=timelevel+1
+  specialindex=specialindex+1;
+end
+%------------------------------------------
+%------------------------------------------
+% compute time averages
+omega_tot_678=squeeze(mean(omega_tot_t,3));
+omega_lon_678=squeeze(mean(omega_lon_t,3));
+omega_lat_678=squeeze(mean(omega_lat_t,3));
+
+% compute zonal and meridional means of the psi-method results:
+omega_lon_678_mm=squeeze(mean(omega_lon_678,2));
+omega_lat_678_mm=squeeze(mean(omega_lat_678,1));
+
+%------------------------------------------
+
+% grab selected areas from the fields calculated in poisson.m:
+clear omega_tot_sch_trop;
+clear omega_lon_sch_trop;
+clear omega_lat_sch_trop;
+% grab the area (5N:35S) of interest -> to histogram
+omega_tot_sch_trop=omega_tot_t(:,lat1:lat2,:);
+omega_lon_sch_trop=omega_lon_t(:,lat1:lat2,:);
+omega_lat_sch_trop=omega_lat_t(:,lat1:lat2,:);
+% average over latitudes
+omega_tot_sch_trop_mn=mean(omega_tot_sch_trop,2);
+omega_lon_sch_trop_mn=mean(omega_lon_sch_trop,2);
+omega_lat_sch_trop_mn=mean(omega_lat_sch_trop,2);
+
+% grab a more traditional definition of tropics: 
+omega_tot_trop=omega_tot_t(:,lat_tr1:lat_tr2,:);
+omega_lon_trop=omega_lon_t(:,lat_tr1:lat_tr2,:);
+omega_lat_trop=omega_lat_t(:,lat_tr1:lat_tr2,:);
+
+% grab the area (110E:160E; maritime cont: mc) 
+% of interest for the regional HC --> to histogram
+omega_tot_sch_mc=omega_tot_t(lon1:lon2,:,:);
+omega_lat_sch_mc=omega_lat_t(lon1:lon2,:,:);
+omega_lon_sch_mc=omega_lon_t(lon1:lon2,:,:);
+
+% grab the values between 30N and 30S:
+omega_tot_hc_mc=omega_tot_sch_mc(:,lat_tr1:lat_tr2,:);
+omega_lat_hc_mc=omega_lat_sch_mc(:,lat_tr1:lat_tr2,:);
+omega_lon_hc_mc=omega_lon_sch_mc(:,lat_tr1:lat_tr2,:);
+
+% calculate the zonal mean
+omega_lon_sch_mc_mn=mean(omega_lon_sch_mc,1);
+omega_lat_sch_mc_mn=mean(omega_lat_sch_mc,1);
+omega_tot_sch_mc_mn=mean(omega_tot_sch_mc,1);
+%omega_tot_sch=omega_total(lon1:lon2,:);
+%omega_lat_sch=omega_hc(lon1:lon2,:);
+%omega_lon_sch=omega_wc(lon1:lon2,:);
+%omega_lon_sch_mn=mean(omega_lon_sch,1);
+%omega_lat_sch_mn=mean(omega_lat_sch,1);
+%omega_tot_sch_mn=mean(omega_tot_sch,1);
+
+%------------------------------------------
+% simple, old-fashioned view of defining the HC and WC as the zonal
+% mean (HC) and zonally asymmetric (WC) overturning circulations:
+% see Vecchi and Soden, 2007 for example
+omega_wc_mm=squeeze(mean(omega_tot_trop,2)); 
+omega_hc_zm=squeeze(mean(omega_tot_trop,1));
+
+figure
+subplot(2,1,1)
+%plot(latitude(lat_tr1:lat_tr2),squeeze(mean(omega_hc_zm,2)),'color',hadley);
+plot(latitude,omega_tot_zm_tm,'color',hadley);
+%hold on
+%plot(latitude(lat_tr1:lat_tr2),squeeze(mean(omega_hc_zm,2)),'color',hadley);
+%plot(latitude(lat_tr1:lat_tr2),squeeze(mean(omega_hc_zm,2)),'color',hadley);
+%xlim([-30 30])
+ylabel('hPa/day')
+xlabel('latitude')
+title('Zonal Mean HC 30N to 30S')
+%figure
+subplot(2,1,2)
+plot(longtude,squeeze(mean(omega_wc_mm,2)),'color',walker);
+%hold on
+%plot(longtude,squeeze(mean(omega_wc_mm,2)),'color',walker);
+%plot(longtude,squeeze(mean(omega_wc_mm,2)),'color',walker);
+xlim([1 360])
+ylabel('hPa/day')
+xlabel('longitude')
+title('Meridional Mean WC, 30N:30S')
+
+% compare old and new methods: 
+figure
+subplot(2,1,1)
+plot(latitude,omega_tot_zm_tm,'color',hadley);
+hold on
+plot(latitude,omega_lat_678_zm,'color',hadley,'LineWidth',1.5);
+ylabel('hPa/day')
+xlabel('latitude')
+title('Hadley Circulations')
+subplot(2,1,2)
+plot(longtude,squeeze(mean(omega_wc_mm,2)),'color',walker);
+hold on
+plot(longtude,omega_lon_678_mm,'color',walker,'LineWidth',1.5);
+xlim([1 360])
+ylabel('hPa/day')
+xlabel('longitude')
+title('Walker Circulations')
+sgtitle('Comparison of Traditional and Psi-method')
+
+%------------------------------------------
+
+%------------------------------------------
+% plot the histograms/PDFs of the omega 500
+% velocity over particular domains 
+
+% histograms on limited area domain: 
+numberb=100;
+xlim1=-80;
+xlim2=80;
+bwide=3
+figure
+subplot(3,1,1)
+h1=histogram(omega_tot_sch_trop);
+title('total Circulation')
+h1.Normalization = 'probability';
+h1.BinWidth = bwide;
+%h1.NumBins = numberb;
+ylim([0 0.12])
+xlim([xlim1 xlim2])
+subplot(3,1,2)
+h2=histogram(omega_lon_sch_trop);
+title('longitudinal contribution: local WC')
+h2.Normalization = 'probability';
+h2.BinWidth = bwide;
+%h2.NumBins = numberb;
+ylim([0 0.12])
+xlim([xlim1 xlim2])
+subplot(3,1,3)
+h3=histogram(omega_lat_sch_trop);
+title('latitudinal contribution: local HC')
+h3.Normalization = 'probability';
+h3.BinWidth = bwide;
+%h3.NumBins = numberb;
+ylim([0 0.12])
+xlim([xlim1 xlim2])
+sgtitle('Regional WC: 5N to 35S')
+
+% histograms over Maritime Continent region: 
+figure
+subplot(3,1,1)
+h1=histogram(omega_tot_hc_mc);
+h1.Normalization = 'probability';
+h1.BinWidth = bwide;
+xlim([xlim1 xlim2])
+subplot(3,1,2)
+h2=histogram(omega_lon_hc_mc);
+h2.Normalization = 'probability';
+h2.BinWidth = bwide;
+xlim([xlim1 xlim2])
+subplot(3,1,3)
+h3=histogram(omega_lat_hc_mc);
+h3.Normalization = 'probability';
+h3.BinWidth = bwide;
+xlim([xlim1 xlim2])
+sgtitle('Regional HC: 110E to 160E')
+
+% histograms on limited area domain: 
+figure
+subplot(3,1,1)
+h1=histogram(omega_tot_trop);
+title('total Circulation')
+h1.Normalization = 'probability';
+h1.BinWidth = bwide;
+ylim([0 0.1])
+xlim([xlim1 xlim2])
+subplot(3,1,2)
+h2=histogram(omega_lon_trop);
+title('longitudinal contribution: local WC')
+h2.Normalization = 'probability';
+h2.BinWidth = bwide;
+ylim([0 0.1])
+xlim([xlim1 xlim2])
+subplot(3,1,3)
+h3=histogram(omega_lat_trop);
+title('latitudinal contribution: local HC')
+h3.Normalization = 'probability';
+h3.BinWidth = bwide;
+ylim([0 0.1])
+xlim([xlim1 xlim2])
+%sgtitle('30N to 30S')
+sgtitle('Tropics: 30N to 30S: months 6,7,8')
+%------------------------------------------
+% regional HC and WC used in Schwendike et al., 2014
+
+figure
+subplot(2,1,1)
+plot(latitude,squeeze(mean(omega_lon_sch_mc_mn,3)),'color',walker);
+hold on
+plot(latitude,squeeze(mean(omega_lat_sch_mc_mn,3)),'color',hadley);
+plot(latitude,squeeze(mean(omega_tot_sch_mc_mn,3)),'k');
+xlim([-90 90])
+ylabel('hPa/day')
+xlabel('latitude')
+title('Regional HC over Maritime Continent')
+subplot(2,1,2)
+plot(longtude,squeeze(mean(omega_lon_sch_trop_mn,3)),'color',walker);
+hold on
+plot(longtude,squeeze(mean(omega_lat_sch_trop_mn,3)),'color',hadley);
+%plot(longtude,omega_lat_sch_trop_mn(:,1,2),'color',hadley);
+%plot(longtude,omega_lat_sch_trop_mn(:,1,3),'color',hadley);
+plot(longtude,squeeze(mean(omega_tot_sch_trop_mn,3)),'k');
+xlim([1 360])
+ylabel('hPa/day')
+xlabel('longitude')
+title('Regional Walker Circulation, 5N:35S')
+
--- poisson.m
+++ poisson.m
@@ -5,36 +5,76 @@
 %
 % I am attempting to compute the partitioning of the 
 % vertical pressure velocity as described by 
-% Schwendike et al., 2014 using the successive over
+% Schwendike et al., 2014 
+%
+% The atmospheric vertical motion is decomposed into 
+% the vertical motion associated with the overturning in 
+% the zonal direction, and the vertical motion associated
+% with the meridional direction.  
+%
+% The solution of the Poisson equation uses a successive over
 % relaxation technique to solve the poisson equation
 % see Fulton et al., 1986 for details of SOR. 
 %
-% starting out very simple on a 10x10 domain
+% I don't understand how best to apply the boundary conditions..
+%
+% is this only solvable up to an arbitrary sign or 
+% additive constant?
+%
+% in matlab, it looks like the function solvepde could 
+% work well, is there a good equivalent in python?
 %
 % levi silvers                       jan 2022
 %--------------------------------------------------
 
+% this controls how many times the solution is 
+% itterated, the number of times needed depends
+% on the desired degree of accuracy and on r.
+errend=30000; % number of itterations for the loop
+
 % input file(s)
-source_file='~/data/CAM/wap500_CFday_CESM2-FV2_historical_r1i1p1f1_gn_20100101-20150101.nc';
-wap500_full=ncread(source_file,'wap500');
+% source_file='~/data/CAM/wap500_CFday_CESM2-FV2_historical_r1i1p1f1_gn_20100101-20150101.nc';
+%wap500_full=ncread(source_file,'wap500');
+
+% below is a file with 3D wap output monthly.
+source_file='~/data/CAM/wap_Amon_CESM2-FV2_amip_r2i1p1f1_gn_200001-201412.nc';
+wap_full=ncread(source_file,'wap');
+level=6; % this should be the 500 hPa level
+wap500_full=squeeze(wap_full(:,:,level,:));
+
+
 latitude = ncread(source_file,'lat');
 longtude = ncread(source_file,'lon');
+press_le = ncread(source_file,'plev');
 
-wap500=wap500_full(:,:,10);
+dlat=latitude(5)-latitude(4); % assuming latitudes are evenly spaced
+dlon=longtude(5)-longtude(4);
+
+
+%grab a particular time...
+%wap500=wap500_full(:,:,10);
+% below for time mean of monthly mean data
+%wap500=mean(wap500_full,3);
+% below is grabbing data from just one month
+
+%timelevel=13
+wap500=squeeze(wap500_full(:,:,timelevel));
 
 
 slon =size(wap500,1);
 slat =size(wap500,2);
 % parameter
 a_earth=6350000;
-rad = pi/180.
+rad = pi/180.;
 r=1.5 % relaxation parameter, if r=1, SOR reduces to Gauss-Seidel method
-errend=100; % number of itterations for the loop
+conv=864.; % (hPa s)/(Pa day); used to convert Pa/s to hPa/day
+
+wap500=conv.*wap500;
 
 % these will need to be modified for use on a sphere, and will then depend on lat
 %h1=1; 
-h1=ones(size(wap500,2));
-h2=ones(size(wap500,1));
+h1=ones(size(wap500,2),1);
+%h2=ones(size(wap500,1),1);
 h1=cos(latitude*rad);
 %h2=2; 
 
@@ -47,25 +87,75 @@ kend=slon;
 % define fields --> eventually f should be an input to the script
 % if we determine that 0 is an appropriate boundary condition (bc), then this 
 % initial array definition also defines bc's.
-u    = zeros(kend,jend); % old values
-un   = zeros(kend,jend); % new values
+u           = zeros(kend,jend); % old values
+un          = zeros(kend,jend); % new values
+omega_wc    = zeros(kend,jend); % new values
+omega_hc    = zeros(kend,jend); % new values
+omega_total = zeros(kend,jend); % new values
 diff = zeros(kend,jend);
 %f    = zeros(kend,jend)+5.;
 f = wap500;
 
-% boundary conditions
+% maybe we should determine the j range here, rather than in the loop below?
+for j=j0:1:jend/2;
+  if abs(latitude(j))<80; % this is probably where BC should be applied
+  %'inside domain'
+  else 
+  %'outside domain'
+  index=j;
+  end
+end
 
-% Loop
 for err=1:errend;
-  for k=k0+1:1:kend-1;
-    for j=j0+1:1:jend-1;
-      un(k,j)=(1-r)*u(k,j)+(r/4.)*((h1(j)*h2(k))*f(k,j)+un(k-1,j)+u(k+1,j)+un(k,j-1)+u(k,j+1));
+  for k=k0+1:1:kend-1; % loop over longitudes
+    %un(k+1,j0+index-1)=un(k+1,j0+index); % BC at (k+1,j0+index-1)
+    for j=j0+index:1:jend-index; % loop over latitudes
+      un(k,j)=(1-r)*u(k,j)+(r/4.)*((h1(j)*dlat*dlon*a_earth*a_earth)*f(k,j)+un(k-1,j)+u(k+1,j)+un(k,j-1)+u(k,j+1));
       diff(k,j)=un(k,j)-u(k,j);
     end
+    % what are the correct BCs at the polar latitudes?  
+    % the solution blows up if we set the value at jend+1 equal to jend
+    %un(k+1,jend-index+1)=un(k0,jend-index); % BC at (k+1,jend-index+1)
+    %un(k+1,j0+index-1)=un(k0,j0+index); % BC at (k+1,j0+index-1)
+    un(k+1,jend-index+1)=0; % BC at (k+1,jend-index+1)
+    un(k+1,j0+index-1)=0; % BC at (k+1,j0+index-1)
   end
-  diff(3,3)
+  %un(k+1,j0+index-1)=un(k+1,j0+index); % BC at (k+1,j0+index-1)
+  % calculate kend value and apply doubly periodic BC to find values at k0
+  for j=j0+index:1:jend-index;
+    un(kend,j)=(1-r)*u(kend,j)+(r/4.)*((h1(j)*dlat*dlon*a_earth*a_earth)*f(kend,j)+un(kend-1,j)+u(k0,j)+un(kend,j-1)+u(kend,j+1));
+    un(k0,j)  =un(kend,j);
+  end
+  % save new approximation for next iteration.  
   u=un;
+end 
+
+% print the error at one arbitrary point
+diff(40,40)
+maxerr=max(max(diff))
+minerr=min(min(diff))
+
+% calculate the longitudinal omega omega_lon(k,j) or (lon,lat)
+% k is the zonal index, j is the meridional index
+% omega_lon(k,j)=(1/(a_earth^2*h1(j)^2))*(partial^2 mu/partial lambda^2)
+for k=k0+1:1:kend-1;
+  %for j=j0+1:1:jend-1; 
+  for j=j0+index:1:jend-index; % loop over latitudes
+    omega_wc(k,j)=(-1/(a_earth^2*h1(j)^2))*((un(k+1,j)-2*un(k,j)+un(k-1,j))/dlon^2);
+    omega_hc(k,j)=(-1/(a_earth^2*h1(j)))*((h1(j+1)*un(k,j+1)-2*h1(j)*un(k,j)+h1(j-1)*un(k,j-1))/dlat^2);
+  end
 end
+% BCs at k0 and kend
+for j=j0+index:1:jend-index; % loop over latitudes
+  omega_wc(kend,j)=(-1/(a_earth^2*h1(j)^2))*((un(k0,j)-2*un(kend,j)+un(kend-1,j))/dlon^2);
+  omega_hc(kend,j)=(-1/(a_earth^2*h1(j)))*((h1(j+1)*un(kend,j+1)-2*h1(j)*un(kend,j)+h1(j-1)*un(kend,j-1))/dlat^2);
+  omega_wc(k0,j)=omega_wc(kend,j);
+  omega_hc(k0,j)=omega_hc(kend,j);
+end
+
+omega_total=omega_wc+omega_hc;
+
+
 
 
 
